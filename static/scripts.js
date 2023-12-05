6c50cf350e2e650bab173e799b939f48ef845f1b
e <6410762+e-mit@users.noreply.github.com>
2023-12-05 13:49:51 +0000

    change var to let in several places

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,5 @@
 function addRow(items) {
-    var newRow = document.getElementById("item-table").insertRow();
+    let newRow = document.getElementById("item-table").insertRow();
     for (const key in items) {
         newRow.insertCell().innerHTML = `<input type="text" name="${key}">`;
     }
@@ -7,45 +7,28 @@ function addRow(items) {
 }
 
 function deleteRow(btn) {
-    var row = btn.parentNode.parentNode;
+    let row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
 }
 
 function submitForm() {
-    var table = document.getElementById("item-table");
-    var formData = [];
-    for (var i = 0; i < table.rows.length; i++) {
-        var row = table.rows[i];
-        var rowData = {};
-        for (var j = 0; j < (row.cells.length - 1); j++) {
-            var k = row.cells[j].getElementsByTagName("input")[0].name;
-            console.log(k);
+    let table = document.getElementById("item-table");
+    let formData = [];
+    for (let i = 0; i < table.rows.length; i++) {
+        let row = table.rows[i];
+        let rowData = {};
+        for (let j = 0; j < (row.cells.length - 1); j++) {
+            let k = row.cells[j].getElementsByTagName("input")[0].name;
             rowData[k] = row.cells[j].getElementsByTagName("input")[0].value;
         }
         formData.push(rowData);
     }
 
-    /*
-    console.log(Array.from(document.getElementById("item-table").rows));
-    for (const row in Array.from(document.getElementById("item-table").rows)) {
-      console.log(row);
-      var rowData = {};
-      for (const cell in Array.from(row.cells)) {
-        console.log(cell);
-        k = cell.getElementsByTagName("input")[0].name;
-        rowData[k] = cell.getElementsByTagName("input")[0].value;
-        console.log(k);
-        console.log(rowData);
-      }
-      formData.push(rowData);
-    }
-    */
-
-    var jsonData = JSON.stringify(formData);
+    let jsonData = JSON.stringify(formData);
     console.log(jsonData);
     document.getElementById("json-data").value = jsonData;
 
-    var form = document.getElementById("invoice-form")
+    let form = document.getElementById("invoice-form")
     form.submit();
   }
 
