6a2142bff8f794082bdc804d2e10923b57b00085
e <6410762+e-mit@users.noreply.github.com>
2023-12-19 07:42:51 +0000

    Add rm to test script

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,5 +1,3 @@
 docker build -t emitz/vat-invoice:latest .
 
-PORT=8000 && docker run -p 8080:${PORT} --name vat-invoice-test -e PORT=${PORT} emitz/vat-invoice:latest
-# then do:
-# docker rm vat-invoice-test
+PORT=8000 && docker run -p 8080:${PORT} --name vat-invoice-test -e PORT=${PORT} --rm emitz/vat-invoice:latest
