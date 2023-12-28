8c3cc3de06f96b6a051a60312bfa79c528dc75df
e <6410762+e-mit@users.noreply.github.com>
2023-12-28 14:15:45 +0000

    Use gunicorn conf file.

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,9 +1,11 @@
-# Gunicorn config variables
+# Gunicorn settings for Google cloud Run: one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
 loglevel = "info"
 errorlog = "-"  # stderr
 accesslog = "-"  # stdout
 worker_tmp_dir = "/dev/shm"
-graceful_timeout = 120
-timeout = 120
-keepalive = 5
-workers = 3
+timeout = 0
+workers = 1
+threads = 8
