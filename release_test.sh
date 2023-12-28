9a9f9746ac82e3c94199ad391b9ba88ba974709a
e <6410762+e-mit@users.noreply.github.com>
2023-12-28 14:16:44 +0000

    Remove compose files which are not needed due to using Google cloud run

--- release_test.sh
+++ release_test.sh
@@ -1,5 +1,5 @@
 docker build --target release -t release:latest .
 
-PORT=8000 && docker run -d -p 8080:${PORT} --name release -e PORT=${PORT} release:latest
+PORT=8000 && docker run -d -p 8080:${PORT} --name release -e PORT=${PORT} --rm release:latest
 
 #docker stop -t 0 release
