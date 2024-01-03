807d03ff89aef7ee4f33f35f525fb9558cde5b56
e <6410762+e-mit@users.noreply.github.com>
2024-01-03 13:39:30 +0000

    Printout docker image SHA256 for local release build

--- release_run.sh
+++ release_run.sh
@@ -1,5 +1,8 @@
 docker build --target release -t release:latest .
 
+new_sha=$(docker images --no-trunc --quiet release:latest)
+echo $new_sha
+
 PORT=8000 && docker run -p 8080:${PORT} --name release -e PORT=${PORT} --rm release:latest
 
 # docker stop -t 0 release
