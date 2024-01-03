22e0902a507a9954eeef719348dc6d59260e747b
e <6410762+e-mit@users.noreply.github.com>
2024-01-03 17:55:27 +0000

    Refactor wait_for_workflow_completion.py

--- tests/wait_for_workflow_completion.py
+++ tests/wait_for_workflow_completion.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 import sys
 import time
+from typing import Any
 from io import BytesIO, TextIOWrapper
 from zipfile import ZipFile
 import requests
@@ -19,6 +20,10 @@ WORKFLOW_SUCCESS: str = "success"
 GITHUB_SHA: str = os.environ['GITHUB_SHA']
 REPO_PATH: str = os.environ['REPO_PATH']
 GH_TOKEN: str = os.environ['GH_TOKEN']
+BASE_URL = f"https://api.github.com/repos/{REPO_PATH}/actions/runs"
+HEADERS = {"Accept": "application/vnd.github+json",
+           "Authorization": f"Bearer {GH_TOKEN}",
+           "X-GitHub-Api-Version": GH_API_VERSION}
 
 
 def list_all_yaml_files(dir: Path | str) -> set[str]:
@@ -41,38 +46,46 @@ def list_exclude_files(argv: list[str]) -> set[str]:
     return files
 
 
+def get_workflows() -> list[dict[str, Any]]:
+    """List all workflows for the current commit."""
+    workflow_query = f"{BASE_URL}?head_sha={GITHUB_SHA}"
+    return requests.get(url=workflow_query,
+                        headers=HEADERS).json()['workflow_runs']
+
+
+def get_artifacts(workflow_id: str) -> list[dict[str, Any]]:
+    """List all artifacts for the workflow with the input ID."""
+    artifact_query = f"{BASE_URL}/{workflow_id}/artifacts"
+    return requests.get(url=artifact_query,
+                        headers=HEADERS).json()['artifacts']
+
+
+def workflow_pass(archive_download_url: str) -> bool:
+    """Open the data file in the artifact and check contents."""
+    data = requests.get(url=archive_download_url, headers=HEADERS)
+    with ZipFile(BytesIO(data.content)) as z:
+        with TextIOWrapper(z.open(ARTIFACT_FILENAME), encoding="utf-8") as f:
+            if f.read().strip() == WORKFLOW_SUCCESS:
+                return True
+            return False
+
+
 if __name__ == "__main__":
     required_workflows = (list_all_yaml_files(Path(".github/workflows"))
                           - list_exclude_files(sys.argv))
-
-    base_url = f"https://api.github.com/repos/{REPO_PATH}/actions/runs"
-    workflow_query = f"{base_url}?head_sha={GITHUB_SHA}"
-    headers = {"Accept": "application/vnd.github+json",
-               "Authorization": f"Bearer {GH_TOKEN}",
-               "X-GitHub-Api-Version": GH_API_VERSION}
     start_time = time.time()
     while (time.time() - start_time) < TIMEOUT_SECONDS:
         passed_workflows: set[str] = set()
         failed_workflows: set[str] = set()
-        workflows = requests.get(url=workflow_query,
-                                 headers=headers).json()['workflow_runs']
-        for workflow in workflows:
+        for workflow in get_workflows():
             if workflow['name'] in required_workflows:
                 expected_artifact_name = f"{workflow['name']}_{GITHUB_SHA}"
-                artifact_query = f"{base_url}/{workflow['id']}/artifacts"
-                artifacts = requests.get(url=artifact_query,
-                                         headers=headers).json()['artifacts']
-                for arti in artifacts:
-                    if arti['name'] == expected_artifact_name:
-                        data = requests.get(url=arti['archive_download_url'],
-                                            headers=headers)
-                        with ZipFile(BytesIO(data.content)) as z:
-                            with TextIOWrapper(z.open(ARTIFACT_FILENAME),
-                                               encoding="utf-8") as f:
-                                if f.read().strip() == WORKFLOW_SUCCESS:
-                                    passed_workflows.add(workflow['name'])
-                                else:
-                                    failed_workflows.add(workflow['name'])
+                for artifact in get_artifacts(workflow['id']):
+                    if artifact['name'] == expected_artifact_name:
+                        if workflow_pass(artifact['archive_download_url']):
+                            passed_workflows.add(workflow['name'])
+                        else:
+                            failed_workflows.add(workflow['name'])
         if passed_workflows == required_workflows:
             exit(PASS_RETURN_VALUE)
         if failed_workflows:
