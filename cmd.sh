3c28eb76b57147ee56154c47475ed4f444a40fb8
e <6410762+e-mit@users.noreply.github.com>
2024-06-26 06:49:21 +0100

    Revert "Specify secret keys by external environment variable"
    This reverts commit c8204fd3c7176d6f3e06b35017d8a5c4649b411a.

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+export FLASK_SECRET_KEY=$(openssl rand -base64 32)
+export CSRF_SECRET=$(openssl rand -base64 32)
+
 exec gunicorn --bind :$PORT --conf gunicorn_conf.py app:app
--- release_run.sh
+++ release_run.sh
@@ -2,14 +2,6 @@
 
 docker build --target release -t release:latest .
 
-PORT=8000
-FLASK_SECRET_KEY=$(openssl rand -base64 32)
-CSRF_SECRET=$(openssl rand -base64 32)
-
-docker run -p 8080:${PORT} --name release \
-    -e PORT=${PORT} \
-    -e FLASK_SECRET_KEY=${FLASK_SECRET_KEY} \
-    -e CSRF_SECRET=${CSRF_SECRET} \
-    --rm release:latest
+PORT=8000 && docker run -p 8080:${PORT} --name release -e PORT=${PORT} --rm release:latest
 
 # docker stop -t 0 release
